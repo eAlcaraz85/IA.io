4c1621a70982e450f0b8c0ebe7f1d4a3f5b211d7
ealcaraz <likcos@gmail.com>
2024-11-14 07:14:22 -0600

    update index

--- code/arbolphaser.py
+++ code/arbolphaser.py
@@ -14,6 +14,7 @@ dataset = pd.read_csv(file_path)
 X = dataset.iloc[:, :2]  # Las dos primeras columnas son las características
 y = dataset.iloc[:, 2]   # La tercera columna es la etiqueta
 
+print(X)
 # Dividir los datos en conjunto de entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -25,6 +26,7 @@ clf.fit(X_train, y_train)
 
 
 y_predict = clf.predict(X_test)
+
 print(X_test, y_predict)
 # Exportar el árbol de decisión en formato DOT para su visualización
 dot_data = export_graphviz(clf, out_file=None, 
